a5914651e1646dfe413a59c0956eafe1b5ad038d
daonb <bennydaon@gmail.com>
2012-03-19 23:54:03 +0200

    support local contexts

--- server.py
+++ server.py
@@ -11,20 +11,22 @@ PORT = 8000
 pystache.Loader.template_path = ['templates', 'templates/include']
 
 class MustachServer(BaseHTTPServer.BaseHTTPRequestHandler):
-    context_txt = open('context.yaml').read()
-    context = yaml.load(context_txt)
+    common_context = yaml.load(open('context.yaml').read())
     loader = pystache.Loader()
 
     def do_GET(self):
         parsed_path = urlparse.urlparse(self.path)
         loc = parsed_path.path[1:] # the first char in a '/' so skip it
-        if loc.startswith(self.context['MEDIA_URL']):
+        if loc.startswith(self.common_context['MEDIA_URL']):
             # serve static files
             self.wfile.write(open(loc).read())
         else:
+            context = self.common_context.copy()
+            context.update(yaml.load(open('templates/%s.yaml' % loc).read()))
             template = self.loader.load_template(loc, encoding='utf-8')
-            html = pystache.render(template, self.context)
+            html = pystache.render(template, context)
             self.wfile.write(html.encode('utf-8'))
+
         self.send_response(200) # 200 in an HTTP OK Result Code
         self.end_headers()
 
