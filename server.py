4e828ca635ae5890e81c20dffe3a78d12d49a7e3
daonb <bennydaon@gmail.com>
2012-03-21 09:47:23 +0200

    guessing the content type for a cleaner server

--- server.py
+++ server.py
@@ -5,6 +5,7 @@ import BaseHTTPServer
 import pystache
 import yaml
 import gettext
+import mimetypes
 
 # minimal web server.  serves mustache templates relative to the
 # current `templates` directory.
@@ -22,13 +23,29 @@ class MustachServer(BaseHTTPServer.BaseHTTPRequestHandler):
     def do_GET(self):
         parsed_path = urlparse.urlparse(self.path)
         loc = parsed_path.path[1:] # the first char in a '/' so skip it
-        try:
-            context = self.common_context.copy()
-            template_fn  = 'templates/%s.yaml' % loc
-            if os.path.exists(template_fn):
-                context.update(yaml.load(open(template_fn).read()))
-            context['_'] = lambda x: self.translation.ugettext(x)
+        if os.path.exists(loc):
+            # static file, guess the content-type and serve
+            src = open(loc).read()
+            self.send_response(200)
+            mime, enc = mimetypes.guess_type(loc)
+            if (mime):
+                self.send_header("content-type", mime) 
+
+            self.end_headers()
+            self.wfile.write(src)
+        else:
             template = self.loader.load_template(loc, encoding='utf-8')
+            if not template:
+                self.send_response(500)
+                self.end_headers()
+                return
+
+            context = self.common_context.copy()
+            context_fn  = 'templates/%s.yaml' % loc
+            if os.path.exists(context_fn):
+                context.update(yaml.load(open(context_fn).read()))
+
+            context['_'] = lambda x: self.translation.ugettext(x)
             html = pystache.render(template, context)
             # response headers
             self.send_response(200)
@@ -36,17 +53,6 @@ class MustachServer(BaseHTTPServer.BaseHTTPRequestHandler):
             self.end_headers()
             # and the content
             self.wfile.write(html.encode('utf-8'))
-        except IOError:
-            ''' probably a static file, let's try and read it '''
-            try:
-                src = open(loc).read()
-                self.send_response(200)
-                self.end_headers()
-                self.wfile.write(src)
-            except IOError:
-                '''nothing works, return a 500 '''
-                self.send_response(500)
-                self.end_headers()
 
 # the next few lines are what happens when ran from the shell:
 # $ python server.py
